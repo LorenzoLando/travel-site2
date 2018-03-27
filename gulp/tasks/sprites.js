80504cfd5ac0bc886e7668ea00418b7d364ba9d3
Lorenzo <lorenzo.lando85@gmail.com>
2018-03-27 19:40:08 +0100

    Completed automated sprite generation

--- gulp/tasks/sprites.js
+++ gulp/tasks/sprites.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp'),
 svgSprite = require('gulp-svg-sprite'),
-rename = require('gulp-rename');
+rename = require('gulp-rename'),
+del = require('del');
 
 var config = {
   mode: {
@@ -15,7 +16,16 @@ var config = {
   }
 }
 
-gulp.task('createSprite', function() {
+// e` necessario che prima di creare una nuova sprite di icone la vecchia eventuale sia eliminata
+//cancello la eventuale Sprite folder presente
+
+gulp.task('beginClean', function() {
+//del() accetta un array di cartelle da cancellare
+  return del(['./app/temp/sprite', './app/assets/images/sprites']);
+});
+
+//non vogliamo che createSprote inizi finche BeginClean non ha finito
+gulp.task('createSprite',['beginClean'], function() {
   return gulp.src('./app/assets/images/icons/**/*.svg')
     .pipe(svgSprite(config))
     .pipe(gulp.dest('./app/temp/sprite/'));
@@ -32,4 +42,9 @@ gulp.task('copySpriteCSS', ['createSprite'], function() {
     .pipe(gulp.dest('./app/assets/styles/modules'));
 });
 
-gulp.task('icons', ['createSprite', 'copySpriteGraphic', 'copySpriteCSS']);
+//quando e finito tutto vogliamo cancellare la cartella temp
+gulp.task('endClean',['copySpriteGraphic', 'copySpriteCSS'], function() {
+  return del('./app/temp/sprite');
+});
+
+gulp.task('icons', ['beginClean','createSprite', 'copySpriteGraphic', 'copySpriteCSS', 'endClean']);
