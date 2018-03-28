09e5e4b616bfa505e48ac18bf8ee471aa4f7ff70
Lorenzo <lorenzo.lando85@gmail.com>
2018-03-28 11:45:48 +0100

    css for header

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -4,11 +4,12 @@ autoprefixer = require('autoprefixer'),
 cssvars = require('postcss-simple-vars'),
 nested = require('postcss-nested'),
 cssImport = require('postcss-import'),
-mixins = require('postcss-mixins');
+mixins = require('postcss-mixins'),
+hexrgba = require('postcss-hexrgba');
 
 gulp.task('styles', function() {
   return gulp.src('./app/assets/styles/styles.css')
-  .pipe(postcss([cssImport, mixins, cssvars, nested, autoprefixer]))
+  .pipe(postcss([cssImport, mixins, cssvars, nested, hexrgba, autoprefixer]))
   //su errore ficiamo a gulp di finire l attivita` specificata senza uscire dalla modalita` watch
   .on('error', function(errorInfo){
   	//logga nella console info utili per capire dove e stato l errore
