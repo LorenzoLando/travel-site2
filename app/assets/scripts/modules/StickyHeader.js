02c5baf620a0691e27e9dd2ad9580b1b6cf85606
Lorenzo <lorenzo.lando85@gmail.com>
2018-09-04 18:26:41 +0100

    animation for the header

--- app/assets/scripts/modules/StickyHeader.js
+++ app/assets/scripts/modules/StickyHeader.js
@@ -1,13 +1,25 @@
 import $ from 'jquery';
 import waypoints from '../../../../node_modules/waypoints/lib/noframework.waypoints';
+import smoothScroll from 'jquery-smooth-scroll';
 
 class StickyHeader {
 	constructor() {
 		this.siteHeader = $(".site-header");
 		this.headerTriggerElement = $(".large-hero__title");
 		this.createHeaderWaypoint();
+		//vogliamo selezionare una collezione con tutte le pagine
+		this.pageSections = $(".page-section");
+		//seleziono tutti i link element della primary-nav
+		this.headerLinks = $(".primary-nav a");
+		this.createPageSectionWaypoints();
+		this.addSmoothScrolling();
 	}
 
+	addSmoothScrolling() {
+		this.headerLinks.smoothScroll();
+	}
+	
+	//waypoint per l`header
 	createHeaderWaypoint() {
 		var that = this;
 		new Waypoint({
@@ -20,6 +32,40 @@ class StickyHeader {
 				 that.siteHeader.removeClass("site-header--dark");
 				}
 			}
+		});
+	}
+	
+	//waypoints per le sezioni 
+	createPageSectionWaypoints() {
+		var that = this;
+		this.pageSections.each(function(){
+			var currentPageSection = this;
+			new Waypoint({
+				element: currentPageSection,
+				handler: function(direction) {
+					if(direction == "down") {
+						// matchingHeaderLink = la prima volta e` il primo selettore jquery 
+						var matchingHeaderLink = currentPageSection.getAttribute("data-matching-link");
+						that.headerLinks.removeClass("is-current-link");
+						$(matchingHeaderLink).addClass("is-current-link");
+					} 
+				},
+
+				offset: "18%"
+			});
+			new Waypoint({
+				element: currentPageSection,
+				handler: function(direction) {
+					if(direction == "up") {
+						// matchingHeaderLink = la prima volta e` il primo selettore jquery 
+						var matchingHeaderLink = currentPageSection.getAttribute("data-matching-link");
+						that.headerLinks.removeClass("is-current-link");
+						$(matchingHeaderLink).addClass("is-current-link");
+					} 
+				},
+
+				offset: "-40%"
+			});
 		});
 	}
 }
